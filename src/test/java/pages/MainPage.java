9fb7ce10cfec733c992b34e5c75aa4dec434b228
martin.skarbala <skarbala.martin@gmail.com>
2019-03-22 12:20:38 +0100

    feat: implemented MainPage steps

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,4 +1,53 @@
 package pages;
 
+import static context.AppiumDriverSingleton.getDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
+
 public class MainPage {
+
+  public MainPage logInWithFakeUser() {
+    selectFakeUser();
+    new WebDriverWait(getDriver(), 120)
+        .until(ExpectedConditions.visibilityOfElementLocated(By.id("btnShowHideBilledExpenses")));
+    return this;
+  }
+
+  private void selectFakeUser() {
+    getDriver().findElement(By.id("android:id/text1")).click();
+    getDriver().findElement(By.xpath("//android.widget.ListView/android.widget.CheckedTextView[2]")).click();
+  }
+
+  public MainPage switchToExpenses() {
+
+    MobileElement carPager = getDriver().findElement(By.id("expenses_card_pager"));
+
+    TouchAction touchAction = new TouchAction(getDriver());
+    int carPagerY = carPager.getCenter().y;
+    int carPagerX = carPager.getCenter().x;
+
+    swipeCard(touchAction, carPagerY, carPagerX);
+    return this;
+  }
+
+  private void swipeCard(TouchAction touchAction, int carPagerY, int carPagerX) {
+    touchAction.press(PointOption.point(carPagerX, carPagerY))
+        .moveTo(PointOption.point(carPagerX - 520, carPagerY))
+        .perform()
+        .release();
+  }
+
+  public void clickAddPrivateExpense() {
+    new WebDriverWait(getDriver(), 10)
+        .until(ExpectedConditions.visibilityOfElementLocated(By.id("btnAddPrivateExpense")));
+    getDriver().findElement(By.id("btnAddPrivateExpense")).click();
+  }
+
+
 }
